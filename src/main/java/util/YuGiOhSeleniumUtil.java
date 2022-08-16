adcc07b571d5e1ad49b33b1740acf11334783b24
Drgil666 <2574105038@qq.com>
2022-08-16 17:13:32 +0800

    Feats:修改部分脚本参数。

--- src/main/java/util/YuGiOhSeleniumUtil.java
+++ src/main/java/util/YuGiOhSeleniumUtil.java
@@ -17,10 +17,10 @@ public class YuGiOhSeleniumUtil {
     public static final String URL = "https://tools.kooriookami.top/#/yugioh";
     public static final String CARD_NAME_XPATH = "/html/body/div[1]/div/section/main/div/div/div[2]/div/div[1]/div/div/div[2]/form/div[2]/div[2]/div/div/input";
     public static final String CARD_CLICK_XPATH = "/html/body/div[2]/div[2]/div/div/div[1]/ul/li";
-    public static final String DOWNLOAD_BUTTON = "/html/body/div[1]/div/section/main/div/div/div[2]/div/div[1]/div/div/div[2]/div[1]/div/div[5]/button";
-    public static final String CARD_LANGUAGE = "/html/body/div[1]/div/section/main/div/div/div[2]/div/div[1]/div/div/div[2]/form/div[1]/div[2]/div/div/div/input";
-    public static final String JAPANESE_CARD = "/html/body/div[2]/div[1]/div/div/div[1]/ul/li[3]";
-    public static final String CHINESE_CARD = "/html/body/div[2]/div[1]/div/div/div[1]/ul/li[1]";
+    public static final String DOWNLOAD_BUTTON = "/html/body/div[1]/div/section/main/div/div/div[2]/div[1]/div[1]/div/div/div[2]/div[1]/div/div[5]/button";
+    public static final String CARD_LANGUAGE = "/html/body/div[1]/div/section/main/div/div/div[2]/div[1]/div[1]/div/div/div[2]/form/div[1]/div[2]/div/div/div/div/input";
+    public static final String JAPANESE_CARD = "/html/body/div[2]/div[2]/div/div/div[1]/ul/li[3]";
+    public static final String CHINESE_CARD = "/html/body/div[2]/div[2]/div/div/div[1]/ul/li[1]";
     public static WebDriver driver;
     public static final String CARD_CODE = "/html/body/div[1]/div/section/main/div/div/div[2]/div[1]/div[1]/div/div/div[2]/form/div[16]/div[2]/div/div[1]/div/input";
     public static final String CARD_CODE_CONFIRM = "/html/body/div[1]/div/section/main/div/div/div[2]/div[1]/div[1]/div/div/div[2]/form/div[16]/div[2]/div/div[2]/div/button[1]";
@@ -31,7 +31,7 @@ public class YuGiOhSeleniumUtil {
     }
 
     private static void preChrome() throws InterruptedException {
-        HashMap<String, Object> hashMap = new HashMap<String, Object>(10);
+        HashMap<String, Object> hashMap = new HashMap<>(10);
         hashMap.put("download.default_directory", downloadPath);
         ChromeOptions chromeOptions = new ChromeOptions ();
         chromeOptions.setExperimentalOption("prefs", hashMap);
@@ -39,7 +39,6 @@ public class YuGiOhSeleniumUtil {
         desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
         System.setProperty("webdriver.chrome.driver", Main.FILE_PATH+"/chrome/Application/chromedriver.exe");
         driver = new ChromeDriver (desiredCapabilities);
-        //driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(URL);
         Main.addLine("Chrome初始化完成!");
